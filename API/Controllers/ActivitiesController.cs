d752ea9e9b421891ead6fd01a2c79a1f465c1b8f
Jaiie1 <jarl.mattias@gmail.com>
2021-11-09 08:10:57 +0100

    Crud Activity Is done

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -27,6 +27,13 @@ namespace API.Controllers
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
         }       
 
     }
