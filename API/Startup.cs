4b3de8a5b6cd5a391e27a43a2a162160d6b7d729
Jaiie1 <jarl.mattias@gmail.com>
2021-11-13 00:33:49 +0100

    Clening Startup.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,22 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Persistence;
-using Microsoft.EntityFrameworkCore;
-using Application.Activities;
-using MediatR;
-using AutoMapper;
-using Application.Core;
+using API.Extenstions;
 
 namespace API
 {
@@ -33,26 +20,10 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationsServices(_config);
+            
         } 
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -5,20 +6,22 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {}
+        public class Query : IRequest<List<Activity>> { }
 
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
-            private readonly DataContext _context;
+            private readonly DataContext _context;         
 
             public Handler(DataContext context)
             {
+                
                 _context = context;
             }
 
@@ -28,8 +31,9 @@ namespace Application.Activities
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                return await _context.Activities.ToListAsync();
+            {                                    
+
+                return await _context.Activities.ToListAsync(cancellationToken);
             }
         }
     }
