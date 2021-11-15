4bdfe82feba79e86368bf7660268ee2f767ad2de
Jaiie1 <jarl.mattias@gmail.com>
2021-11-15 15:10:32 +0100

    New react after bugg

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css'
+import App from './app/layout/App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
