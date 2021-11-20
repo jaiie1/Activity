4ce76ba2d7423826f1a158563b9c590b8fedf98f
Jaiie1 <jarl.mattias@gmail.com>
2021-11-20 09:20:12 +0100

    Child component and navbar and list complete

---
+++ client-app/src/app/feature/ActivityDashBoard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import ActivityList from "./activites/dashboard/ActivityList";
+
+interface Props {
+    activities: Activity[];
+}
+
+
+export default function ActivityDashBoard({ activities }: Props) {
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList activities={activities} />
+            </Grid.Column>
+        </Grid>
+    )
+}
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import  './App';
 import axios from 'axios';
 import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashBoard from '../feature/ActivityDashBoard';
 
 function App () {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useState(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(res => {
         console.log(res);
         setActivities(res.data);
       })
   }, );
   return (
-    <div>     
-      <Header as='h2' icon='users' content='Activity' />
-
-       <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>{activity.title}{activity.description}
-            
-
-            </List.Item>
-          ))}
-        </List>        
-    </div>
+    <>     
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+      <ActivityDashBoard activities={activities} />
+      </Container>             
+    </>
   );
  }
 
